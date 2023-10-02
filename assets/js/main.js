35c5844874ceba02b75c60343d346fbd756560ec
Oliver Schwill <o.schwill@web.de>
2023-10-02 10:47:21 +0200

    Project init

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const gameElementsUI = {
+  rock: '<i class="fa-solid fa-hand-fist" style="color: #fff400;"></i>',
+  scissor: '<i class="fa-solid fa-hand" style="color: #fff400;"></i>',
+  paper: '<i class="fa-solid fa-hand-scissors fa-rotate-90" style="color: #fff400;"></i>',
+};
+
+/* PROPERTIES */
+let maxRounds = 0;
+let playerScore = 0;
+let cpuScore = 0;
